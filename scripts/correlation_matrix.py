513d72c9d3a44ff8909f0fe34de7481ef831b7f1
biotep <biotep@yahoo.com>
2018-09-29 00:30:51 +0800

    First commit of Gemini

---
+++ scripts/correlation_matrix.py
@@ -0,0 +1,8 @@
+from bokeh.plotting import figure
+from bokeh.models import ColumnDataSource, Panel, Slope, PreText
+from bokeh.layouts import column, row
+import numpy as np
+
+class Correlation_matrix:
+    def __init__(self, data):
+        pass
